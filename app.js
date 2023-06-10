b01c93285e4bba72e504be753b252aed6c970205
Dhruv <prashardhruv2229@gmail.com>
2023-06-10 10:36:54 +0530

    cloudinary setup

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const express = require('express');
 const app = express();
 
 const fileUpload = require('express-fileupload');
+const cloudinary= require('cloudinary').v2
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+})
 
 // database
 const connectDB = require('./db/connect');
@@ -20,6 +26,7 @@ app.use(express.static('./public'));
 app.use(express.json());
 app.use(fileUpload({
   limits:{ fileSize: 50 * 1024 * 1024 },
+  useTempFiles:true
 }));
 
 app.get('/', (req, res) => {
